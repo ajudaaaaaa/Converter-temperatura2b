28d4e477aceffae782e4f57a9a40b095125f1749
2024105070057 <2024105070057@aqs-lab-c-18.mshome.net>
2025-04-23 14:49:38 -0300

    Primeiro Commit

---
+++ src/main/java/poo/convertter_temperatura/Convertter_temperatura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.convertter_temperatura;
+
+/**
+ *
+ * @author 2024105070057
+ */
+public class Convertter_temperatura {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
